9466ffdae099e899b2f743ac80a9d30eba416c2e
zuhaumar <zuhaumar2@gmail.com>
2023-10-08 17:05:33 +0500

    Fixed sql file

--- database.sql
+++ database.sql
@@ -6,8 +6,7 @@ create table students(
 		StudentID int,
         FirstName varchar(50),
         LastName varchar(50),
-        Primary key(StudentID)
-        
+        Primary key(StudentID)   
 );
 Insert INTO students(StudentID,FirstName,LastName)
 Values(1,"Fatima","Abdul Wahid"),(2,"Zuha","Umar"),(3,"Hadiya","Farooq");
